8e6e7bfb56240102ffbf5e3c5cc158403636b326
Akshay Jain <51874814+akshayjain777@users.noreply.github.com>
2023-02-05 09:38:20 -0500

    Add files via upload

---
+++ Projectss/crawl social media data and visualize it using graphs/keys.py
@@ -0,0 +1,7 @@
+consumer_key = "NyflgnLci5M8TdvdXc5JxnTt5"
+
+consumer_secret = "Xt8Q6T5RfVeNh4PcZvpdIzJcXasp2lfJWq0jK45XKAIGrnjka9"
+
+access_token = "1487290900179034113-DVpySVAr9gtPCatc9bl8x6oRVyuRPm"
+
+access_token_secret = "2wtUkX6n4wkgzdKcC06GDNpGjgOeIWqH1DuOORCWg0Fud"
